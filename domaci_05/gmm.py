f77ea61dd38c6e9123b4e0d577d2134a2af26610
Janko <ljubicjanko1@gmail.com>
2020-05-27 00:07:08 +0200

    result 6

--- domaci_05/gmm.py
+++ domaci_05/gmm.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.mixture import GaussianMixture
 from sklearn.metrics import v_measure_score
 
@@ -35,13 +34,14 @@ def read(filePath, training=False):
     # read data
     data = pd.read_csv(filePath)
 
-    # plt.figure(figsize=(7, 7))
-    # plt.scatter(data["infant"], data["region"])
-    # plt.xlabel('infant')
-    # plt.ylabel('region')
-    # plt.title('Infant by region')
-    # plt.show()
-
+    if training:
+        data = data[data['infant'] < 350]
+        plt.figure(figsize=(7, 7))
+        plt.scatter(data["infant"], data["region"])
+        plt.xlabel('infant')
+        plt.ylabel('region')
+        plt.title('Infant by region')
+        plt.show()
     # plt.figure(figsize=(7, 7))
     # plt.scatter(data["income"], data["region"])
     # plt.xlabel('income')
@@ -72,25 +72,29 @@ def read(filePath, training=False):
     return x, y
 
 if __name__ == '__main__':
-    X_train, y_train = read("./resources/train.csv")
+    X_train, y_train = read("./resources/train.csv", True)
     # X_test, y_test = read("./resources/test_preview.csv")
     X_test, y_test = read("./resources/test_final.csv")
 
     maximum = 0
     max_n = 0
     iteration = 0
-    for n in range(2, 80):
+
+    for n in range(2, 60):
         for i in range(10):
             g = GaussianMixture(n_components=n)
             g.fit(X_train, y_train)
 
+
             y_pred = g.predict(X_test)
+
             v_measure = v_measure_score(y_test, y_pred)
             if v_measure > maximum:
                 iteration = i
                 maximum = v_measure
                 max_n = n
+            if maximum == 1.0:
+                break
 
     print("N components: " + str(max_n) + " Iteration: " + str(iteration))
-    print("#"*20)
     print(maximum)
